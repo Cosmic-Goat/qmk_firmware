3818bf2b8c795c292ccf70f36ffdf12e4e105f87
Shrey Pasricha <shrey.pasricha@gmail.com>
2020-03-04 20:12:30 -0500

    Added more keys to layout.

--- keyboards/planck/keymaps/cosmicgoat/keymap.c
+++ keyboards/planck/keymaps/cosmicgoat/keymap.c
@@ -53,7 +53,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_ESC,  KC_Q,    KC_W,    KC_F,    KC_P,    KC_G,    KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN, KC_SLSH,
     KC_TAB,  KC_A,    KC_R,    KC_S,    KC_T,    KC_D,    KC_H,    KC_N,    KC_E,    KC_I,    KC_O,    KC_BSPC,
     KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_K,    KC_M,    KC_DOT,  KC_COMM, KC_QUOT, KC_ENT,
-    KC_LCTL, KC_LCTL, KC_LGUI, KC_LALT, LOWER,   KC_SPC,  KC_SPC,  RAISE,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
+    KC_LCTL, KC_RCTL, KC_LGUI, KC_LALT, LOWER,   KC_SPC,  KC_SPC,  RAISE,   KC_RSHIFT, KC_RALT, KC_PGDN,   KC_PGUP
 ),
 
 /* Qwerty
@@ -86,10 +86,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------------------------------------------------'
  */
 [_LOWER] = LAYOUT_planck_grid(
-    KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_7,    KC_8, KC_9,    KC_RPRN, KC_BSPC,
-    _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_4,    KC_5, KC_6,    _______, KC_DEL,
-    _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_1,    KC_2, KC_3,    KC_END,  _______,
-    _______, _______, _______, _______, _______, _______, _______, _______, KC_0, KC_VOLD, KC_VOLU, KC_MPLY
+    KC_TILD, _______, _______,   _______, _______,  _______, _______, KC_7,    KC_8, KC_9,    KC_0, _______,
+    _______, _______,   KC_GRAVE,   KC_PIPE,   KC_BSLS,   _______,   _______,   KC_4,    KC_5, KC_6,    KC_MINS, KC_DEL,
+    _______, _______,   _______,   _______,   _______,  _______,  _______,  KC_1,    KC_2, KC_3,    KC_EQL,  _______,
+    _______, _______, _______, _______, _______, _______, _______, _______, KC_0, _______, _______, _______
 ),
 
 /* Raise
@@ -105,9 +105,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [_RAISE] = LAYOUT_planck_grid(
     KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,
-    KC_DEL,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_LEFT, KC_UP,   KC_RGHT, KC_DOWN, KC_BSLS,
-    _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_NUHS, KC_NUBS, KC_PGUP, KC_PGDN, _______,
-    _______, _______, _______, _______, _______, _______, _______, _______, KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY
+    _______,  KC_LPRN,   KC_RPRN,   KC_LBRC,   KC_RBRC,   _______,   KC_INS,   KC_LEFT, KC_UP,   KC_RGHT, KC_DOWN, KC_DEL,
+    _______, _______,   _______,   _______,   _______,  _______,  _______,  KC_PGUP, KC_PGDN, KC_HOME, KC_END, _______,
+    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
 ),
 
 /* Adjust (Lower + Raise)
@@ -123,7 +123,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------------------------------------------------'
  */
 [_ADJUST] = LAYOUT_planck_grid(
-    _______, RESET,   DEBUG,   RGB_TOG, RGB_MOD, RGB_HUI, RGB_HUD, RGB_SAI, RGB_SAD,  RGB_VAI, RGB_VAD, KC_DEL ,
+    _______, RESET,   DEBUG,   RGB_TOG, RGB_MOD, RGB_HUI, RGB_HUD, RGB_SAI, RGB_SAD,  RGB_VAI, RGB_VAD, _______ ,
     _______, _______, MU_MOD,  AU_ON,   AU_OFF,  AG_NORM, AG_SWAP, COLEMAK,  QWERTY,  _______,  _______,  _______,
     _______, MUV_DE,  MUV_IN,  MU_ON,   MU_OFF,  MI_ON,   MI_OFF,  TERM_ON, TERM_OFF, _______, _______, _______,
     _______, _______, _______, _______, _______, _______, _______, _______, _______,  _______, _______, _______
@@ -218,21 +218,21 @@ void dip_switch_update_user(uint8_t index, bool active) {
     switch (index) {
         case 0: {
 #ifdef AUDIO_ENABLE
-            static bool play_sound = false;
+      //      static bool play_sound = false;
 #endif
             if (active) {
 #ifdef AUDIO_ENABLE
-                if (play_sound) { PLAY_SONG(plover_song); }
+                //if (play_sound) { PLAY_SONG(plover_song); }
 #endif
                 layer_on(_ADJUST);
             } else {
 #ifdef AUDIO_ENABLE
-                if (play_sound) { PLAY_SONG(plover_gb_song); }
+                //if (play_sound) { PLAY_SONG(plover_gb_song); }
 #endif
                 layer_off(_ADJUST);
             }
 #ifdef AUDIO_ENABLE
-            play_sound = true;
+   //         play_sound = true;
 #endif
             break;
         }
